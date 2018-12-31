178b4e3c1cb4435ebfd37774c9e0498babc83d68
Kiran <kiranbanmala3@gmail.com>
2018-12-31 13:06:06 +0545

    assignment inheritance

---
+++ OOP/inherit_Assignment.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+
+class College:
+    def __init__(self):
+        self.collegeName = "Khwopa"
+        self.location = "Bkt"
+
+class Faculty(College):
+    def __init__(self,fac):
+        self.facultyName = fac
+
+class Teacher(Faculty):
+    def __init__(self,name,period,salary,fac):
+        self.name = name
+        self.period = period
+        self.salary = salary
+        Faculty.__init__(self,fac)
+
+    def info(self):
+        print("Name \t: ",self.name)
+        print("Period \t: ",self.period)
+        print("Salary \t: ",self.salary)
+        print("Faculty\t: ",self.facultyName,"\n")
+
+class Student(Faculty):
+    def __init__(self,name,sub,mark,fac):
+        self.name = name
+        self.subject = sub
+        self.mark = mark
+        Faculty.__init__(self,fac)
+
+    def result(self):
+        if self.mark >= 90:
+            print("Excellent!")
+        elif self.mark >= 80:
+            print("Good!")
+        elif self.mark >= 60:
+            print("Satisfactory!")
+        else:
+            print("Not Good")
+
+    def info(self):
+        print("Name \t: ",self.name)
+        print("Subject\t: ",self.subject)
+        print("Marks \t: ",self.mark)
+
+teach1 = Teacher("Roshan",4,30000,"Computer")
+teach1.info()
+
+stud1 = Student("Kiran","Physics",55,"Computer")
+stud1.info()
+print("Remarks\t: ",end=" ")
+stud1.result()
