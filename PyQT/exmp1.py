f420067935aa6e19823286e514b8277b94b3cc1c
Kiran <kiranbanmala3@gmail.com>
2019-01-06 18:11:58 +0545

    trial file

--- PyQT/exmp1.py
+++ PyQT/exmp1.py
@@ -1,25 +1,60 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QMessageBox, QLabel
 
-
-class App(QWidget):
+class App(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 simple window - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
+        self.title = 'Addition_App'
+        self.left = 400
+        self.top = 100
+        self.width = 300
+        self.height = 300
         self.initUI()
 
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        #Create Label 1
+        self.label1 = QLabel(self)
+        self.label1.setText("First Num : ")
+        self.label1.move(20,20)
+
+        # Create textbox 1
+        self.firstNum = QLineEdit(self)
+        self.firstNum.move(100, 20)
+        self.firstNum.resize(100, 20)
+
+        # Create Label 2
+        self.label2 = QLabel(self)
+        self.label2.setText("Second Num : ")
+        self.label2.move(20, 50)
+
+        # Create textbox 2
+        self.secNum = QLineEdit(self)
+        self.secNum.move(100, 50)
+        self.secNum.resize(100, 20)
+
+        # Create a button in the window
+        self.button = QPushButton('ADD', self)
+        self.button.move(100, 100)
+
+        # connect button to function on_click
+        self.button.clicked.connect(self.on_click)
         self.show()
+
+
+    #@pyqtSlot()
+    def on_click(self):
+        fNum = int(self.firstNum.text())
+        sNum = int(self.secNum.text())
+        sum =fNum + sNum
+        QMessageBox.question(self, 'Results: ', "Sum of two numbers is :  " + str(sum), QMessageBox.Ok, QMessageBox.Ok)
+        self.firstNum.setText("")
+        self.secNum.setText("")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-ex = App()
-sys.exit(app.exec_())
+    ex = App()
+    sys.exit(app.exec_())
---
+++ PyQT/trial.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5 import QtWidgets
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    l1 = QtWidgets.QLabel(w)
+    l1.move(100, 20)
+    f1 = QtWidgets.QLineEdit(w)
+    f1.move(180, 20)
+
+    l2 = QtWidgets.QLabel(w)
+    l2.setText("Last Name: ")
+    l2.move(100, 60)
+    f2 = QtWidgets.QLineEdit(w)
+    f2.move(180, 60)
+
+    l3 = QtWidgets.QLabel(w)
+    l3.setText("Age: \t")
+    l3.move(100, 100)
+    f3 = QtWidgets.QLineEdit(w)
+    f3.move(180, 100)
+
+    b1 = QtWidgets.QPushButton('OK',w)
+    b1.move(180,140)
+
+    w.setWindowTitle("Form")
+    w.setGeometry(400, 100, 600, 400)
+
+    w.show()
+    sys.exit(app.exec_())
+
+window()
